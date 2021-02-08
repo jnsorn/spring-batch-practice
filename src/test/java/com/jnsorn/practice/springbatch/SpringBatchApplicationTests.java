509eb35ec01f3eddb4efebb1be9b721e8d8883ae
jnsorn <jnsorn@woowahan.com>
2021-02-08 23:49:42 +0900

    init : initial commit

---
+++ src/test/java/com/jnsorn/practice/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnsorn.practice.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
